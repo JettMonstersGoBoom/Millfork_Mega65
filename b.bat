2516d808a650168479f8a1ab908462bbca4d90bf
Jett <jed.adams@gmail.com>
2021-05-27 14:37:21 -0400

    added keyboard registers renamed some includes to fit together as one unit fixed silly temp vbl stuff

--- b.bat
+++ b.bat
@@ -1,3 +1,3 @@
 
-millfork-0.3.28\millfork.exe -t mega65_small -fsource-in-asm -s -g -i include mega.dmatest.mfk -o bin\dmatest
+millfork-0.3.28\millfork.exe -t mega65_small -fsource-in-asm -s -g -i include m65.dmatest.mfk -o bin\dmatest
 
