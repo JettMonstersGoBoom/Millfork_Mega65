461a58f276c3b0bd07b21d2e44698f487c2565e8
Jett <jed.adams@gmail.com>
2021-05-31 10:04:19 -0400

    added basic raster irq

---
+++ b_irq.bat
@@ -0,0 +1,3 @@
+
+millfork-0.3.28\millfork.exe -t mega65_small -fsource-in-asm -s -g -i include m65.irq.mfk -o bin\irqtest
+
---
+++ r_irq.bat
@@ -0,0 +1,2 @@
+rem m65tools\m65 -l COM6 -F -r bin\16color.prg
+m65tools\m65 -l COM6 -F -r bin\irqtest.prg 
